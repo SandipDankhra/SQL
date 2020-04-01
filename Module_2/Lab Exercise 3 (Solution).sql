f4a73d001d21015755d19fc495ee6b2d2dad8c43
Dankhra sandip <39943347+SandipDankhra@users.noreply.github.com>
2020-04-01 19:25:58 +0530

    Update Lab Exercise 3 (Solution).sql

--- Module_2/Lab Exercise 3 (Solution).sql
+++ Module_2/Lab Exercise 3 (Solution).sql
@@ -13,16 +13,16 @@ GO
 -- Write a script to create a table to store the Competitor data.
 ---------------------------------------------------------------------
 CREATE TABLE DesignMarketing.Competitors(
-	CompetitorId int PRIMARY KEY IDENTITY (1, 1),
+	CompetitorId INT PRIMARY KEY IDENTITY (1, 1),
 	 CompetitorName VARCHAR(30) NOT NULL,
 	 Address VARCHAR(MAX) NOT NULL,
 	 CompetitionBrand VARCHAR(50) NOT NULL,
-	 DateEntered datetime NOT NULL,
+	 DateEntered DATETIME NOT NULL,
 	 StrengthOfCompetition VARCHAR(10) NOT NULL,
 	 Comments VARCHAR(MAX) NULL
 );
 
-drop TABLE DesignMarketing.Competitor;
+DROP TABLE DesignMarketing.Competitors;
 
 
 
@@ -33,18 +33,18 @@ drop TABLE DesignMarketing.Competitor;
 -- Write a script to create a table to store the TVAdvertisement data.
 ---------------------------------------------------------------------
 CREATE TABLE DesignMarketing.TVAdvertisement(
-	TVAdvertisementId int NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	TVAdvertisementId INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
     TVAdvertisementName VARCHAR(50) NOT NULL,
     TVAdvertisementProduct VARCHAR(50) NOT NULL,
 	TVAdvertisementBrand VARCHAR(50) NOT NULL,
     TVChannelName VARCHAR(50) NOT NULL,
-	TVAdvertisementDate DateTime NOT NULL,
+	TVAdvertisementDate DATETIME NOT NULL,
 	TVAdvertisementTime VARCHAR(50) NOT NULL,
 	TVAdvertisementCost MONEY NOT NULL,
 );
 
 
-drop TABLE DesignMarketing.TVAdvertisement;
+DROP TABLE DesignMarketing.TVAdvertisement;
 
 
 ---------------------------------------------------------------------
@@ -53,7 +53,7 @@ drop TABLE DesignMarketing.TVAdvertisement;
 -- Write a script to create a table to store the CampaignResponse data.
 ---------------------------------------------------------------------
 CREATE TABLE DesignMarketing.CampaignResponse(
-	CampaignId int NOT NULL PRIMARY KEY IDENTITY(1,1),
+	CampaignId INT NOT NULL PRIMARY KEY IDENTITY(1,1),
     CampaignName VARCHAR(50) NOT NULL,
 	CampaignResponse VARCHAR(10) NOT NULL,
     CampaignCity VARCHAR(50) NOT NULL,
@@ -63,4 +63,4 @@ CREATE TABLE DesignMarketing.CampaignResponse(
     CampaignProfit MONEY NOT NULL,
 );
 
-drop TABLE DesignMarketing.CampaignResponse;
+DROP TABLE DesignMarketing.CampaignResponse;
