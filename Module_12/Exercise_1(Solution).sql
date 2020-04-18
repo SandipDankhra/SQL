32532abf35c68de05bd6757ab6dbad23c9a87a0f
Dankhra sandip <39943347+SandipDankhra@users.noreply.github.com>
2020-04-18 10:11:11 +0530

    Module_12 has added

---
+++ Module_12/Exercise_1(Solution).sql
@@ -0,0 +1,27 @@
+use InternetSales
+
+alter database InternetSales
+add filegroup SalesFG contains
+MEMORY_OPTIMIZED_DATA
+Go
+
+ALTER DATABASE InternetSales   
+ADD FILE(  
+    NAME =N'Data',  
+    FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL14.SQLEXPRESS\MSSQL\DATA\Data' 
+)  
+TO FILEGROUP SalesFG;  
+
+
+CREATE TABLE dbo.ShoppingCart
+(SessionID INT NOT NULL,
+TimeAdded DATETIME NOT NULL,
+CustomerKey INT NOT NULL,
+ProductKey INT NOT NULL,
+Quantity INT NOT NULL
+PRIMARY KEY NONCLUSTERED (SessionID, ProductKey)) 
+WITH  (MEMORY_OPTIMIZED = ON,  DURABILITY = SCHEMA_AND_DATA);
+
+
+INSERT INTO ShoppingCart(SessionID,TimeAdded,CustomerKey,ProductKey,Quantity)VALUES (1,GETDATE(),2,3,1);
+INSERT INTO ShoppingCart(SessionID,TimeAdded,CustomerKey,ProductKey,Quantity)VALUES (2,GETDATE(),2,4,3);
